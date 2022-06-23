3514ecd06cd37b6c27215cb9d811ec9a8f509f5c
CatOstrovsky <shamaaaaaaaag@gmail.com>
2022-06-23 23:25:45 +0300

    ScriptableObjects example

---
+++ Assets/ScriptableObjects/SlicesConfiguration.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SlicesConfig", menuName = "ScriptableObjects/CreateSlicesConfig", order = 1)]
+public class SlicesConfiguration : ScriptableObject
+{
+    [Header("Slices configuration")]
+    [Tooltip("Ключ - тип слайса, значение - на сколько градусов его необходимо развернуть в зависимости от позиции")]
+    public List<SliceRule> rotationOnKindConfig = new List<SliceRule>();
+}
